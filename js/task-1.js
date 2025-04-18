526123240ddbad46be3904e2925946ccbdae7f1b
Yana Rudometova <rudometova.ya@gmail.com>
2025-04-18 14:32:47 +0300

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const elements = document.querySelectorAll(".item");
+console.log(`Number of categories: ${elements.length}`);
+
+elements.forEach(element => {
+    const category = element.querySelector('h2').textContent;
+    const elementsLi = element.querySelectorAll('li').length;
+
+    console.log(`Category: ${category}`);
+    console.log(`Elements: ${elementsLi}`);
+    
+    
+});
