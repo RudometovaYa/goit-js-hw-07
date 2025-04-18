da876571dbc9ef0b207e325b2e738d0b13a7e176
Yana Rudometova <rudometova.ya@gmail.com>
2025-04-18 19:24:47 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const email = e.target.elements.email;
+    const password = e.target.elements.password;
+    
+    if (email.value.trim() === "" || password.value.trim() === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const data = {
+    userEmail: e.target.elements.email.value.trim(),
+    userPassword: e.target.elements.password.value.trim(),
+  };
+
+  
+  console.log(data);
+    
+  e.target.reset();
+});
